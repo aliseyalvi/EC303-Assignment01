747dcb02ff33772adb0948d167921fffb4a06b90
Sadaqat Ali <muhammadalisialvi@gmail.com>
2021-03-14 10:05:49 +0500

    lab04 task1

--- lib/main.dart
+++ lib/main.dart
@@ -1,195 +1,221 @@
-import 'dart:ui';
+/// Flutter code sample for Scaffold
+
+// This example shows a [Scaffold] with a [body] and [FloatingActionButton].
+// The [body] is a [Text] placed in a [Center] in order to center the text
+// within the [Scaffold]. The [FloatingActionButton] is connected to a
+// callback that increments a counter.
+//
+// ![The Scaffold has a white background with a blue AppBar at the top. A blue FloatingActionButton is positioned at the bottom right corner of the Scaffold.](https://flutter.github.io/assets-for-api-docs/assets/material/scaffold.png)
 
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(const MyApp());
 
+/// This is the main application widget.
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  const MyApp({Key? key}) : super(key: key);
+
+  static const String _title = 'Flutter Code Sample';
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Login UI',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        // visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Login'),
+      title: _title,
+      initialRoute: '/',
+      routes: {
+        '/': (context) => HomeScreen(),
+        '/profile': (context) => ProfileScreen(),
+        '/notifications': (context) => NotificationScreen(),
+        '/settings': (context) => SettingsScreen(),
+      },
+      // home: MyStatefulWidget(),
     );
   }
 }
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-  final String title;
+class HomeScreen extends StatelessWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  TextStyle style = TextStyle(fontFamily: 'Montserrat', fontSize: 20.0);
-  bool _obscureText = true;
-  String _password;
-  bool value = false;
-  bool _passwordVisible = false;
-  void _toggle() {
-    setState(() {
-      _obscureText = !_obscureText;
-    });
-  }
-
-  @override
-  Widget buildCheckbox() => ListTile(
-        leading: Checkbox(
-          value: value,
-          onChanged: (value) {
-            setState(() {
-              this.value = value;
-            });
-          },
-        ),
-        title: Text(
-          'Remember Me',
-          style: const TextStyle(fontSize: 12.0),
-        ),
-      );
-
   Widget build(BuildContext context) {
-    bool checkBoxValue = true;
-    final emailField = TextFormField(
-      decoration: const InputDecoration(
-          labelText: 'Email',
-          hintText: 'Enter Your Email',
-          icon: const Padding(
-              padding: const EdgeInsets.only(top: 15.0),
-              child: const Icon(Icons.email))),
-      // obscureText: _obscureText,
-    );
-    final passwordField = TextField(
-      obscureText: true,
-      style: style,
-      decoration: InputDecoration(
-          contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-          hintText: "Password",
-          border:
-              OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
-    );
-    final loginButon = Material(
-      elevation: 5.0,
-      borderRadius: BorderRadius.circular(30.0),
-      color: Colors.blueAccent,
-      child: MaterialButton(
-        minWidth: MediaQuery.of(context).size.width,
-        padding: EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 10.0),
-        onPressed: () {},
-        child: Text("Login",
-            textAlign: TextAlign.center,
-            style: style.copyWith(
-                color: Colors.white, fontWeight: FontWeight.bold)),
-      ),
-    );
-
     return Scaffold(
-      resizeToAvoidBottomInset: false,
       appBar: AppBar(
-        title: Text('Login'),
-        backgroundColor: Colors.blueAccent,
+        title: Text('Home Screen'),
       ),
+      drawer: HomeDrawer(),
       body: Center(
-        child: Container(
-          color: Colors.white70,
-          child: Padding(
-            padding: const EdgeInsets.all(25.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                // SizedBox(
-                //   height: 155.0,
-                //   child: Image.network(
-                //     'https://st3.depositphotos.com/12985790/18246/i/600/depositphotos_182461084-stock-photo-anonymous.jpg',
-                //     fit: BoxFit.contain,
-                //   ),
-                // ),
-                CircleAvatar(
-                    radius: 75,
-                    backgroundImage: NetworkImage(
-                        'https://st3.depositphotos.com/12985790/18246/i/600/depositphotos_182461084-stock-photo-anonymous.jpg')),
-                SizedBox(height: 45.0),
-                emailField,
-                TextFormField(
-                  keyboardType: TextInputType.text,
-                  obscureText:
-                      !_passwordVisible, //This will obscure text dynamically
-                  decoration: InputDecoration(
-                    labelText: 'Password',
-                    hintText: 'Enter your password',
-                    icon: const Padding(
-                        padding: const EdgeInsets.only(top: 15.0),
-                        child: const Icon(Icons.lock)),
-                    suffixIcon: IconButton(
-                      icon: Icon(
-                        // Based on passwordVisible state choose the icon
-                        _passwordVisible
-                            ? Icons.visibility
-                            : Icons.visibility_off,
-                        color: Theme.of(context).primaryColorDark,
-                      ),
-                      onPressed: () {
-                        // Update the state i.e. toogle the state of passwordVisible variable
-                        setState(() {
-                          _passwordVisible = !_passwordVisible;
-                        });
-                      },
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height: 35.0,
-                ),
-                Row(
-                  children: <Widget>[
-                    Expanded(child: buildCheckbox()),
-                    Expanded(
-                        child: Text(
-                      'Forget Password',
-                      style: TextStyle(color: Colors.blue),
-                      textAlign: TextAlign.end,
-                    ))
-                  ],
-                ),
-                // SizedBox(
-                loginButon,
-                SizedBox(
-                  height: 15.0,
-                ),
-              ],
+        child: Text('Home Screen'),
+      ),
+    );
+  }
+}
+
+class ProfileScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Profile Screen'),
+      ),
+      drawer: HomeDrawer(),
+      body: Center(
+        child: Text('Profile Screen'),
+      ),
+    );
+  }
+}
+
+class NotificationScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Notifications Screen'),
+      ),
+      drawer: HomeDrawer(),
+      body: Center(
+        child: Text('Notifications Screen'),
+      ),
+    );
+  }
+}
+
+class SettingsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Settings Screen'),
+      ),
+      drawer: HomeDrawer(),
+      body: Center(
+        child: Text('Settings Screen'),
+      ),
+    );
+  }
+}
+
+
+class HomeDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+        child: ListView(children: <Widget>[
+      Container(
+        padding: EdgeInsets.only(top: 24, left: 8, right: 8, bottom: 16),
+        color: Colors.white,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            ClipRRect(
+              borderRadius: BorderRadius.circular(100.0),
+              child: Image.network(
+                'https://i.pinimg.com/736x/3f/94/70/3f9470b34a8e3f526dbdb022f9f19cf7.jpg',
+                width: 80,
+                height: 80,
+                fit: BoxFit.fill,
+              ),
             ),
-          ),
+
+            RichText(
+                text: TextSpan(children: [
+              TextSpan(
+                  text: "Sadaqat Ali\n",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Montserrat',
+                      color: Colors.black87)),
+              TextSpan(
+                  text: "sali.bscs17seecs@seecs.edu.pk",
+                  style: TextStyle(
+                      fontFamily: 'Montserrat', color: Colors.black54)),
+            ]))
+          ],
         ),
+      ),
+      Divider(
+        height: 1,
+        thickness: 1,
+        color: Colors.blueGrey[900],
+      ),
+      Container(
+        height: MediaQuery.of(context).size.height,
+        color: Colors.white,
+        child: Column(
+          children: <Widget>[
+            ListTile(
+              dense: true,
+              title: Text(
+                "Home",
+                style: TextStyle(color: Colors.black),
+              ),
+              leading: Icon(Icons.home),
+              onTap: () {
+                Navigator.pushNamed(context, "/");
+              },
+            ),
+            ListTile(
+              dense: true,
+              title: Text(
+                "Profile",
+                style: TextStyle(color: Colors.black),
+              ),
+              leading: Icon(Icons.person),
+              onTap: () {
+                Navigator.pushNamed(context, '/profile');
+              },
+            ),
+            ListTile(
+              title: Text(
+                "Notifications",
+                style: TextStyle(color: Colors.black),
+              ),
+              leading: Icon(Icons.notifications),
+              onTap: () {
+                Navigator.pushNamed(context, "/notifications");
+              },
+            ),
+            ListTile(
+              dense: true,
+              title: Text(
+                "Settings",
+                style: TextStyle(color: Colors.black),
+              ),
+              leading: Icon(Icons.settings),
+              onTap: () {
+                Navigator.pushNamed(context, "/settings");
+              },
+            ),
+          ],
+        ),
+      ),
+    ]));
+  }
+}
+
+
+/// This is the stateful widget that the main application instantiates.
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({Key? key}) : super(key: key);
+
+  @override
+  _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
+}
+
+/// This is the private State class that goes with MyStatefulWidget.
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  int _count = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sample Code'),
+      ),
+      drawer: HomeDrawer(),
+      body: Center(child: Text('You have pressed the button $_count times.')),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => setState(() => _count++),
+        tooltip: 'Increment Counter',
+        child: const Icon(Icons.add),
       ),
     );
   }
