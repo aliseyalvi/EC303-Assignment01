1bde2b2f8b37af57c4956a90147a96fcb6f7e4a4
Sadaqat Ali <muhammadalisialvi@gmail.com>
2021-03-31 15:26:49 +0500

    lab05 task1

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,71 +11,351 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Login UI',
       theme: ThemeData(
-        primarySwatch: Colors.indigo,
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+        // This makes the visual density adapt to the platform that you run
+        // the app on. For desktop platforms, the controls will be smaller and
+        // closer together (more dense) than on mobile platforms.
+        // visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Sadaqat Ali',),
+      // home: MyHomePage(title: 'Login'),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => LoginPage(title: 'Login'),
+        '/home': (context) => HomeScreen()
+      },
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key,required this.title}) : super(key: key);
+class LoginPage extends StatefulWidget {
   // MyHomePage({Key key, this.title}) : super(key: key);
+  const LoginPage({key, this.title}) : super(key: key);
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
   final String title;
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _LoginPageState createState() => _LoginPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  String text_value = 'Hello User';
-  change() {
+class _LoginPageState extends State<LoginPage> {
+  TextStyle style = TextStyle(fontFamily: 'Montserrat', fontSize: 20.0);
+  bool _obscureText = true;
+  String _password = "";
+  String _username = "";
+  bool value = false;
+  bool _passwordVisible = false;
+  final _formKey = GlobalKey<FormState>();
+
+  void _toggle() {
     setState(() {
-      text_value = 'Hello 217967';
+      _obscureText = !_obscureText;
     });
   }
 
   @override
-  Widget build(BuildContext context) {
-    final textfield = Container(
-      padding: EdgeInsets.all(16),
-      child: Text('$text_value', style: TextStyle(fontSize: 24)),
-    );
-    final button = Material(
-      elevation: 3,
-      borderRadius: BorderRadius.horizontal(),
-      child: ElevatedButton(
-        onPressed: () {
-          change();
-        },
-        child: Text(
-          'Press',
-          textAlign: TextAlign.center,
-          style: TextStyle(fontSize: 24),
-          
+  Widget buildCheckbox() => ListTile(
+        leading: Checkbox(
+          value: value,
+          onChanged: (value) {
+            setState(() {
+              this.value = value;
+            });
+          },
         ),
+        title: Text(
+          'Remember Me',
+          style: const TextStyle(fontSize: 12.0),
+        ),
+      );
+
+  Widget build(BuildContext context) {
+    bool checkBoxValue = true;
+    final emailField = TextFormField(
+      onChanged: (text) {
+        setState(() {
+          this._username = text;
+        });
+      },
+      decoration: const InputDecoration(
+          labelText: 'User Name',
+          hintText: 'Enter Your User Name',
+          icon: const Padding(
+              padding: const EdgeInsets.only(top: 15.0),
+              child: const Icon(Icons.email))),
+      // obscureText: _obscureText,
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return 'Please enter username';
+        }
+        return null;
+      },
+    );
+    final passwordField = TextField(
+      obscureText: true,
+      style: style,
+      decoration: InputDecoration(
+          contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+          hintText: "Password",
+          border:
+              OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
+    );
+    final loginButon = Material(
+      elevation: 5.0,
+      borderRadius: BorderRadius.circular(30.0),
+      color: Colors.blueAccent,
+      child: MaterialButton(
+        minWidth: MediaQuery.of(context).size.width,
+        padding: EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 10.0),
+        onPressed: () {
+          if (_formKey.currentState.validate()) {
+            Navigator.pushNamed(context, "/home",
+                arguments: ScreenArguments(_username));
+          }
+        },
+        child: Text("Login",
+            textAlign: TextAlign.center,
+            style: style.copyWith(
+                color: Colors.white, fontWeight: FontWeight.bold)),
       ),
     );
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Sadaqat Ali'),
-        backgroundColor: Colors.blueAccent,
-        leading: Row(
-          children: [
-            Container(
-              child: CircleAvatar(
-                backgroundImage: NetworkImage('https://i.ibb.co/DbGHyY4/IMG-20190507-143957-2.jpg'),
 
-              ),
-            )
-          ],
-        ),
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        title: Text('Login'),
+        backgroundColor: Colors.blueAccent,
       ),
       body: Center(
-        child: Column(
-          children: <Widget>[textfield, button],
+        child: Container(
+          color: Colors.white70,
+          child: Padding(
+            padding: const EdgeInsets.all(25.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                // SizedBox(
+                //   height: 155.0,
+                //   child: Image.network(
+                //     'https://st3.depositphotos.com/12985790/18246/i/600/depositphotos_182461084-stock-photo-anonymous.jpg',
+                //     fit: BoxFit.contain,
+                //   ),
+                // ),
+                CircleAvatar(
+                    radius: 75,
+                    backgroundImage: NetworkImage(
+                        'https://st3.depositphotos.com/12985790/18246/i/600/depositphotos_182461084-stock-photo-anonymous.jpg')),
+                SizedBox(height: 45.0),
+                Form(
+                  key: _formKey,
+                  child: Column(
+                    children: [
+                      emailField,
+                      TextFormField(
+                        keyboardType: TextInputType.text,
+                        obscureText:
+                            !_passwordVisible, //This will obscure text dynamically
+                        decoration: InputDecoration(
+                          labelText: 'Password',
+                          hintText: 'Enter your password',
+                          icon: const Padding(
+                              padding: const EdgeInsets.only(top: 15.0),
+                              child: const Icon(Icons.lock)),
+                          suffixIcon: IconButton(
+                            icon: Icon(
+                              // Based on passwordVisible state choose the icon
+                              _passwordVisible
+                                  ? Icons.visibility
+                                  : Icons.visibility_off,
+                              color: Theme.of(context).primaryColorDark,
+                            ),
+                            onPressed: () {
+                              // Update the state i.e. toogle the state of passwordVisible variable
+                              setState(() {
+                                _passwordVisible = !_passwordVisible;
+                              });
+                            },
+                          ),
+                        ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter password';
+                          }
+                          return null;
+                        },
+                      ),
+                      SizedBox(
+                        height: 35.0,
+                      ),
+                      Row(
+                        children: <Widget>[
+                          Expanded(child: buildCheckbox()),
+                          Expanded(
+                              child: Text(
+                            'Forget Password',
+                            style: TextStyle(color: Colors.blue),
+                            textAlign: TextAlign.end,
+                          ))
+                        ],
+                      ),
+                      // SizedBox(
+                      loginButon,
+                      SizedBox(
+                        height: 15.0,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
+  }
+}
+
+class ScreenArguments {
+  final String title;
+
+  ScreenArguments(this.title);
+}
+
+class HomeScreen extends StatefulWidget {
+  // MyHomePage({Key key, this.title}) : super(key: key);
+  const HomeScreen({key, this.title}) : super(key: key);
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+  final String title;
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  TextStyle style = TextStyle(fontFamily: 'Montserrat', fontSize: 20.0);
+  static const routeName = '/home';
+  final _formKey = GlobalKey<FormState>();
+  double _firstNumber = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    final ScreenArguments args = ModalRoute.of(context).settings.arguments;
+
+    final firstField = TextFormField(
+      keyboardType: TextInputType.number,
+      onChanged: (text) {
+        setState(() {
+          this._firstNumber = text as double;
+        });
+      },
+      decoration: const InputDecoration(
+          labelText: 'First Number',
+          hintText: 'Enter First Number Here',
+          ),
+      // obscureText: _obscureText,
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return 'Please enter a valid number';
+        }
+        return null;
+      },
+    );
+
+    final secondfField = TextFormField(
+      keyboardType: TextInputType.number,
+      onChanged: (text) {
+        setState(() {
+          this._firstNumber = text as double;
+        });
+      },
+      decoration: const InputDecoration(
+        labelText: 'Second Number',
+        hintText: 'Enter Second Number Here',
+      ),
+      // obscureText: _obscureText,
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return 'Please enter a valid number';
+        }
+        return null;
+      },
+    );
+
+    final proceedButton = Material(
+      elevation: 5.0,
+      borderRadius: BorderRadius.circular(30.0),
+      color: Colors.blueAccent,
+      child: MaterialButton(
+        minWidth: MediaQuery.of(context).size.width,
+        padding: EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 10.0),
+        onPressed: () {
+          if (_formKey.currentState.validate()) {
+            // Navigator.pushNamed(context, "/home",
+            //     arguments: ScreenArguments(_username));
+          }
+        },
+        child: Text("Proceed",
+            textAlign: TextAlign.center,
+            style: style.copyWith(
+                color: Colors.white, fontWeight: FontWeight.bold)),
+      ),
+    );
+
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(args.title),
+        ),
+        body: Center(
+          child: Container(
+            padding: EdgeInsets.all(25),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Form(
+                  key: _formKey,
+                  child: Column(
+                    children: [
+                      firstField,
+                      secondfField,
+                      SizedBox(
+                        height: 35.0,
+                      ),
+
+                      // SizedBox(
+                      proceedButton,
+                      SizedBox(
+                        height: 15.0,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ));
   }
 }
