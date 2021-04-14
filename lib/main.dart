7f16dc56fc2b1f33cdcde6449a8278c0c6bf9680
Sadaqat Ali <muhammadalisialvi@gmail.com>
2021-04-14 22:51:33 +0500

    lab07

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter/cupertino.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,289 +7,225 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Login UI',
-      initialRoute: '/',
-      routes: {
-        '/': (context) => resume(),
-        // '/': (context) => MyApp(),
-      },
-      theme: ThemeData(
-        primarySwatch: Colors.red,
-      ),
-      // home: MyHomePage(title: 'Flutter Login'),
+    return (MaterialApp(
+      home: recipebook(),
+    ));
+  }
+}
+
+class recipebook extends StatefulWidget {
+  @override
+  _recipebookState createState() => _recipebookState();
+}
+
+class _recipebookState extends State<recipebook> {
+
+  final List<String> recepieImages = <String>['burger', 'pizza'];
+  final List<String> recepieNames = <String>['burger', 'pizza'];
+  final List<String> recepiePrep = <String>['30', '50'];
+  final List<String> recepieDesc = <String>['This is burger', 'This is Pizza'];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: Colors.teal,
+          actions: [IconButton(icon: Icon(Icons.book), onPressed: () {})],
+          title: Text(
+            'My Recipe Book',
+            style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+          )),
+      body: SafeArea(
+          child: ListView.separated(
+        padding: const EdgeInsets.all(8),
+        itemCount: recepieImages.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Container(
+            height: 50,
+            // color: Colors.amber[colorCodes[index]],
+            // child: Center(child: Text('Entry ${entries[index]}')),
+            child: InkWell(
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  Expanded(
+                    flex: 1,
+                    child: CircleAvatar(
+                      radius: 30,
+                      backgroundImage:
+                          AssetImage('images/${recepieImages[index]}.jpg'),
+                    ),
+                  ),
+                  Expanded(
+                    flex: 3,
+                    child: Column(
+                      children: [
+                        Expanded(
+                          flex: 1,
+                          child: Row(
+                            children: <Widget>[
+                              Text(
+                                recepieNames[index],
+                                style: TextStyle(
+                                  fontSize: 22,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                        Expanded(
+                          flex: 1,
+                          child: Row(
+                            children: <Widget>[
+                              Text(
+                                'Prep Time: ${recepiePrep[index]} min',
+                                style: TextStyle(fontSize: 16),
+                              )
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => recipedetail(
+                            recipename: recepieImages[index],
+                            recipeimg: recepieNames[index],
+                            preptime: recepiePrep[index],
+                            recipedescription: recepieDesc[index])));
+
+              },
+            ),
+          );
+        },
+        separatorBuilder: (BuildContext context, int index) => const Divider(),
+      )),
     );
   }
 }
 
-class resume extends StatefulWidget {
+class recipedetail extends StatefulWidget {
+  static const String routeName = '/recipedetail';
+  String recipename;
+  String recipeimg;
+  String preptime;
+  String recipedescription;
+  recipedetail(
+      {this.recipedescription, this.preptime, this.recipeimg, this.recipename});
+
   @override
-  _resumeState createState() => _resumeState();
+  _recipedetailState createState() => _recipedetailState();
 }
 
-class _resumeState extends State<resume> {
-  bool get hasFocus => false;
-  @override
-  Widget build(BuildContext context) {
-    final personal_info = MaterialButton(
-      minWidth: MediaQuery.of(context).size.width,
-      padding: EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 10.0),
-      onPressed: () {
-        Navigator.push(
-            context, MaterialPageRoute(builder: (context) => personal()));
-      },
-      child: Text(
-        "Personal Information",
-        textAlign: TextAlign.center,
-        style: TextStyle(color: Colors.red),
-      ),
-    );
-
-    final cover_letter = MaterialButton(
-      minWidth: MediaQuery.of(context).size.width,
-      padding: EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 10.0),
-      onPressed: () {
-        Navigator.push(
-            context, MaterialPageRoute(builder: (context) => coverletter()));
-      },
-      child: Text(
-        "Cover Letter",
-        textAlign: TextAlign.center,
-        style: TextStyle(color: Colors.red),
-      ),
-    );
-
-    return new Scaffold(
-        appBar: AppBar(
-          title: Text("Resume Builder"),
+class _recipedetailState extends State<recipedetail> {
+  Expanded fullrecipe() {
+    return Expanded(
+      child: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+              flex: 3,
+              child: Container(
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage('images/${widget.recipeimg}.jpg'),
+                        fit: BoxFit.fill)),
+                alignment: Alignment.center,
+                padding: EdgeInsets.all(20),
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Container(
+                decoration: BoxDecoration(border: Border.all()),
+                child: Column(
+                  children: [
+                    Expanded(
+                      flex: 1,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Text(
+                            '${widget.recipename}',
+                            style: TextStyle(
+                              fontSize: 30,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      flex: 1,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Text(
+                            'Prep Time: ${widget.preptime} min',
+                            style: TextStyle(fontSize: 20),
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+                flex: 3,
+                child: Container(
+                  child: Column(
+                    children: [
+                      Expanded(
+                        child: Row(children: <Widget>[
+                          Text(
+                            'Complete Recipe Description: ',
+                            style: TextStyle(fontSize: 20),
+                          )
+                        ]),
+                      ),
+                      Expanded(
+                        flex: 3,
+                        child: Container(
+                            width: double.infinity,
+                            child: Text(
+                              '${widget.recipedescription}',
+                              style: TextStyle(fontSize: 18),
+                            )),
+                      )
+                    ],
+                  ),
+                ))
+          ],
         ),
-        body: ListView(
-          children: <Widget>[personal_info, cover_letter],
-        ));
-  }
-}
-
-class personal extends StatefulWidget {
-  @override
-  _personalState createState() => _personalState();
-}
-
-class _personalState extends State<personal> {
-  bool _value = false;
-  void changevalue(bool value) {
-    setState(() {
-      _value = value;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Personal Information'),
-      ),
-      body: Center(
-        child: ListView(children: [
-          Container(
-            color: Colors.grey.shade300,
-            child: Column(
-              children: [
-                Card(
-                  color: Colors.grey.shade300,
-                  child: ListTile(
-                    leading: Text(
-                      'Name:',
-                      style: TextStyle(
-                        color: Colors.grey.shade800,
-                        fontSize: 20,
-                      ),
-                    ),
-                  ),
-                ), //name
-                TextField(
-                  decoration: InputDecoration(
-                    labelText: 'User Name',
-                    hintText: 'Enter Your User Name',
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-                //name
-                Card(
-                  color: Colors.grey.shade300,
-                  child: ListTile(
-                    leading: Text(
-                      'Address:',
-                      style: TextStyle(
-                        color: Colors.grey.shade800,
-                        fontSize: 20,
-                      ),
-                    ),
-                  ),
-                ), //address
-                TextField(
-                  decoration: InputDecoration(
-                    labelText: 'Address',
-                    hintText: 'Enter Your Address',
-                    border: OutlineInputBorder(),
-                  ),
-                ), //address
-                Card(
-                  color: Colors.grey.shade300,
-                  child: ListTile(
-                    leading: Text(
-                      'Email:',
-                      style: TextStyle(
-                        color: Colors.grey.shade800,
-                        fontSize: 20,
-                      ),
-                    ),
-                  ),
-                ), //email
-                TextField(
-                  decoration: InputDecoration(
-                    labelText: 'Email',
-                    hintText: 'Enter Your Email',
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-                Card(
-                  color: Colors.grey.shade300,
-                  child: ListTile(
-                    leading: Text(
-                      'Number:',
-                      style: TextStyle(
-                        color: Colors.grey.shade800,
-                        fontSize: 20,
-                      ),
-                    ),
-                  ),
-                ), //email
-                TextField(
-                  decoration: InputDecoration(
-                    labelText: 'Number',
-                    hintText: 'Enter Your Number',
-                    border: OutlineInputBorder(),
-                  ),
-                ), //email
-                const SizedBox(
-                  height: 12,
-                ),
-                new SwitchListTile(
-                    activeColor: Colors.red,
-                    activeTrackColor: Colors.yellow,
-                    title: Text('Photo'),
-                    value: _value,
-                    onChanged: (bool value) {
-                      changevalue(value);
-                    }),
-                // Image.asset(_value ? 'images/mypic.jpeg' : '', height: 100),
-                CircleAvatar(
-                    radius: 75,
-                    backgroundImage: NetworkImage(
-                        'https://st3.depositphotos.com/12985790/18246/i/600/depositphotos_182461084-stock-photo-anonymous.jpg')),
-              ],
-            ),
-          ),
-        ]),
       ),
     );
   }
-}
-
-class coverletter extends StatefulWidget {
-  @override
-  _coverletterState createState() => _coverletterState();
-}
-
-class _coverletterState extends State<coverletter> {
-  bool _value = false;
-  void changevalue(bool value) {
-    setState(() {
-      _value = value;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        title: Text('Cover Letter'),
+        centerTitle: true,
+        backgroundColor: Colors.teal,
+        title: Text(
+          widget.recipename,
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+        ),
       ),
-      body: Center(
-        child: ListView(children: [
-          Container(
-            color: Colors.grey.shade300,
-            child: Column(
-              children: [
-                new SwitchListTile(
-                    title: Text(
-                      'Cover Letter',
-                      style:
-                          TextStyle(fontSize: 20, color: Colors.grey.shade800),
-                    ),
-                    value: _value,
-                    onChanged: (bool value) {
-                      changevalue(value);
-                    }),
-                Card(
-                  color: Colors.grey.shade300,
-                  child: ListTile(
-                    leading: Text(
-                      'Date:',
-                      style: TextStyle(
-                        color: Colors.grey.shade800,
-                        fontSize: 20,
-                      ),
-                    ),
-                  ),
-                ), //email
-                TextField(
-                  decoration: InputDecoration(
-                    hintText: 'Enter date',
-                    border: OutlineInputBorder(),
-                  ),
-                ), //address
-                Card(
-                  color: Colors.grey.shade300,
-                  child: ListTile(
-                    leading: Text(
-                      'Addressed To ( Optional ) :',
-                      style: TextStyle(
-                        color: Colors.grey.shade800,
-                        fontSize: 20,
-                      ),
-                    ),
-                  ),
-                ), //email
-                TextField(
-                  decoration: InputDecoration(
-                    hintText: 'Enter Address',
-                    border: OutlineInputBorder(),
-                  ),
-                ), //email
-                Card(
-                  color: Colors.grey.shade300,
-                  child: ListTile(
-                    leading: Text(
-                      'Body :',
-                      style: TextStyle(
-                        color: Colors.grey.shade800,
-                        fontSize: 20,
-                      ),
-                    ),
-                  ),
-                ), //email
-                TextField(
-                  decoration: InputDecoration(
-                    fillColor: Colors.white,
-                    hintText: 'Enter Body',
-                    border: OutlineInputBorder(),
-                  ),
-                ), //email
-              ],
-            ),
-          ),
-        ]),
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[fullrecipe()],
+        ),
       ),
     );
   }
